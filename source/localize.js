e84138e4ad975b93fb154d976be56a44cd152b34
WiggelMc <kim.hh@outlook.de>
2022-04-28 17:33:34 +0200

    update source

---
+++ source/localize.js
@@ -0,0 +1,32 @@
+const langDict = {
+    "German": {
+        "##test1": "Deutsch",
+        "##submit": "Senden"
+    },
+    "English": {
+        "##test1": "English",
+        "##submit": "Submit"
+    }
+}
+const lang = langDict[paramLang] ?? langDict.English;
+
+
+const localElements = document.getElementsByClassName("local");
+const localPropertyElements = document.getElementsByClassName("localP");
+
+for (const elem of localElements) {
+    const str = elem.innerHTML;
+    elem.innerHTML = lang[str];
+}
+
+for (const elem of localPropertyElements) {
+    const str = elem.getAttribute(elem.dataset.local);
+    elem.setAttribute("value", lang[str]);
+}
+
+/*
+    Examples:
+     <elem value="##translation_key" class="localP" data-local="value">
+     <elem placeholder="##translation_key" class="localP" data-local="placeholder">
+     <elem class="local">##translation_key</elem>
+*/
